c698b33a5b308f309ffef7c28edd3d8e8218fd74
Jesse Welnick <jesse.welnick@turner.com>
2018-09-01 11:26:55 -0400

    typo

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 const configSchema = joi.object({
   DEFAULT_CITY: joi.string().default('Atlanta'),
-  PATH: joi.number().integer().default(3000),
+  PORT: joi.number().integer().default(3000),
   REDIS_HOST: joi.string().required(),
   REDIS_PORT: joi.number().integer().default(6379),
   WEATHER_DAILY_REDIS_CACHE_TIMEOUT: joi.number().integer().default(60 * 30),
